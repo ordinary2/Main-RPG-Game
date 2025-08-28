784fdec572fd4d9b3bb2895a6168e6dc5c23d2d1
Irakli <izaridze699@gmail.com>
2025-08-28 19:21:48 +0400

    add movement,visauls and animations

---
+++ Assets/EntityState.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public abstract class EntityState
+{
+    protected Player player;
+    protected StateMachine stateMachine;
+    protected string animBoolName;
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public EntityState(Player player, StateMachine stateMachine, string animBoolName)
+    {
+        this.player = player;
+        this.stateMachine = stateMachine;
+        this.animBoolName = animBoolName;
+
+        anim = player.anim;
+        rb = player.rb;
+    }
+
+    public virtual void Enter()
+    {
+        anim.SetBool(animBoolName, true);
+    }
+
+    public virtual void Update()
+    {
+        Debug.Log("I run update of " + animBoolName);
+    }
+
+    public virtual void Exit()
+    {
+        anim.SetBool(animBoolName, false);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    
+    private PlayerInputSet input;
+    private StateMachine stateMachine;
+    
+    public Player_IdleState idleState { get; private set; }
+    public Player_MoveState moveState { get; private set; }
+
+    public Vector2 moveInput { get; private set; }
+
+    [Header("Movement details")] 
+    public float moveSpeed;
+
+    private bool facingRight = true;
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        
+        stateMachine = new StateMachine();
+        input = new PlayerInputSet();
+
+        idleState = new Player_IdleState(this, stateMachine, "idle");
+        moveState = new Player_MoveState(this, stateMachine, "move");
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+
+        input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Start()
+    {
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.UpdateActiveState();
+    }
+
+    public void SetVelocity(float xVelocity, float yVelocity)
+    {
+        rb.linearVelocity = new Vector2(xVelocity, yVelocity);
+        HandleFlip(xVelocity);
+    }
+
+    private void HandleFlip(float xVelocity)
+    {
+        if (xVelocity > 0 && facingRight == false)
+            Flip();
+        else if(xVelocity < 0 && facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+}
